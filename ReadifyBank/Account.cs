376315644ee74d2e84a6902641071efdb1537658
goodluck-yh <goodluck.yh2014@gmail.com>
2017-03-25 16:37:08 +1100

    Create Account

---
+++ ReadifyBank/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadifyBank
+{
+    class Account : Interfaces.IAccount
+    {
+        private string accountNumber, customerName;
+        private decimal balance;
+        private DateTimeOffset openedDate;
+
+        public Account(string accountNumber, string customerName, decimal balance, DateTimeOffset openedDate)
+        {
+            this.accountNumber = accountNumber;
+            this.customerName = customerName;
+            this.balance = balance;
+            this.openedDate = openedDate;
+        }
+        public string AccountNumber
+        {
+            get
+            {
+                return accountNumber;
+            }
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+        }
+
+        public string CustomerName
+        {
+            get
+            {
+                return customerName;
+            }
+        }
+
+        public DateTimeOffset OpenedDate
+        {
+            get
+            {
+                return openedDate;
+            }
+        }
+    }
+}
